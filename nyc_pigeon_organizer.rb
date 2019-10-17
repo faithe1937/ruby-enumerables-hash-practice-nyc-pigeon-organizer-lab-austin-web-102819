6b2e8f4d60e8ff88e79720f5bf8d74c1c040b471
Faith Janicek <faithe1937@gmail.com>
2019-10-17 18:15:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,43 @@ def nyc_pigeon_organizer(data)
   final_hash = {}
 
 data.each do |attribute_hash, block|
-  
-  
+  block.each do |key_category, array|
+    array.each do |name|
+      final_hash[name] = {:color => [], :gender => [], :lives => []}
+    end
+  end
+end
+
+new_array = final_hash.keys
+data[:color].each do |bird_color, name|
+  name.each do |bird_name|
+    new_array.each do |item|
+      if bird_name === item 
+        final[item][:color] << bird_color.to_s 
+      end
+    end
+  end
+end
+data[:gender].each do |gender, girl_boy|
+  girl_boy.each do |bird_name|
+    new_array do |item|
+      if bird_name === item
+        final[item] [:gender] << gender.to_s
+      end
+    end
+  end
+end
+
+data[:lives].each do |location, loc_name|
+  loc_name.each do |bird_name|
+    new_array.each do |item|
+      if bird_name === item 
+        final[item][:lives] << location
+      end
+    end
+  end
+end
+
+return final_hash  
   
 end
